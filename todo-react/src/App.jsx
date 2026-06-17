411fdba1fc786233506e33a82b7d93a795497688
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-17 17:10:22 +0500

    conditinal rendering in jsx

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,18 +1,13 @@
 const userName = 'Magzhan';
+let isLoggedIn = false;
 
 const App = () => {
   return (
     // using <> instead of <div> to return several tags with no additional wrapper
     <>
       <h1 className="title">To Do List</h1>
-      <p style={{ color: 'red', fontWeight: 700 }}>Hi, {userName}!</p>
-      <p>{1 + 1}</p>
-      <p>{userName.toUpperCase()}</p>
-      <p>{new Date().toLocaleDateString()}</p>
-      {/* comment */}
-      <hr />
-      <label htmlFor="email"></label>
-      <input id="email" type="email" required />
+      {isLoggedIn && <p>Hi, {userName}!</p>}
+      <p>{isLoggedIn ? `Hi, ${userName}!` : 'Please Log In!'}</p>
     </>
   );
 };
