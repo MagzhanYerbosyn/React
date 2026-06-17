099bd7a4ce8eb7fc6ae0ee0435fe97adb91a5709
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-17 17:03:49 +0500

    updated basic syntax for jsx

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -4,8 +4,15 @@ const App = () => {
   return (
     // using <> instead of <div> to return several tags with no additional wrapper
     <>
-      <h1>To Do List</h1>
-      <p>Hi, {userName}!</p>
+      <h1 className="title">To Do List</h1>
+      <p style={{ color: 'red', fontWeight: 700 }}>Hi, {userName}!</p>
+      <p>{1 + 1}</p>
+      <p>{userName.toUpperCase()}</p>
+      <p>{new Date().toLocaleDateString()}</p>
+      {/* comment */}
+      <hr />
+      <label htmlFor="email"></label>
+      <input id="email" type="email" required />
     </>
   );
 };
