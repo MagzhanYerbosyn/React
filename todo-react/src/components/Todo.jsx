ee5ea1881e7db3b9c0a909cd9d7ede17d8a44ef8
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-18 22:00:49 +0500

    feat: add search option to the search field in form

--- todo-react/src/components/Todo.jsx
+++ todo-react/src/components/Todo.jsx
@@ -21,11 +21,15 @@ const Todo = () => {
     console.log(`Task with id: ${taskId} is ${isDone ? 'complete' : 'not complete'}`);
   };
 
+  const filterTasks = (query) => {
+    console.log(`Search: ${query}`);
+  };
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
-      <SearchTaskForm />
+      <SearchTaskForm onSearchInput={filterTasks} />
       <TodoInfo
         total={tasks.length}
         done={tasks.filter(({ isDone }) => isDone).length}
