68116f098bd2dfa4b6683371904fc21df19b2f27
Yerbossyn <erbosynmagzan@gmail.com>
2026-06-25 15:24:24 +0500

    refactor: add map method to return different cards

--- favorite-movie-card/src/components/MovieList.tsx
+++ favorite-movie-card/src/components/MovieList.tsx
@@ -1,23 +1,36 @@
 import MovieItem from './MovieItem';
 
 const MovieList = () => {
+  const movieArr = [
+    {
+      name: 'Joker',
+      year: 2019,
+      link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVOWx5p_WcRojfeAqqW-wmqxnPzK-6R2PDjyP2TmCB47zhKoHjylZco8M&s=10',
+    },
+    {
+      name: 'Avatar: The Way of Water',
+      year: 2022,
+      link: 'https://m.media-amazon.com/images/M/MV5BNWI0Y2NkOWEtMmM2OC00MjQ3LWI1YzItZGQxYzQ3NzI4NWZmXkEyXkFqcGc@._V1_.jpg',
+    },
+    {
+      name: 'End of Watch',
+      year: 2012,
+      link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGzt7NKMcF9yFtCEo5nB8U5iQTJlTNo8wRi7ZDGQMwyA&s=10',
+    },
+  ];
+
   return (
     <div className="movie-list">
-      <MovieItem
-        movieName="Joker"
-        releaseDate={2019}
-        posterLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVOWx5p_WcRojfeAqqW-wmqxnPzK-6R2PDjyP2TmCB47zhKoHjylZco8M&s=10"
-      />
-      <MovieItem
-        movieName="Avatar: The Way of Water"
-        releaseDate={2022}
-        posterLink="https://m.media-amazon.com/images/M/MV5BNWI0Y2NkOWEtMmM2OC00MjQ3LWI1YzItZGQxYzQ3NzI4NWZmXkEyXkFqcGc@._V1_.jpg"
-      />
-      <MovieItem
-        movieName="End of Watch"
-        releaseDate={2012}
-        posterLink="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGzt7NKMcF9yFtCEo5nB8U5iQTJlTNo8wRi7ZDGQMwyA&s=10"
-      />
+      {movieArr.map((movie) => {
+        return (
+          <MovieItem
+            key={`${movie.name}-${movie.year}`}
+            movieName={movie.name}
+            releaseDate={movie.year}
+            posterLink={movie.link}
+          />
+        );
+      })}
     </div>
   );
 };
