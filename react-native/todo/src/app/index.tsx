1e33f166063f7ecafa6e96b52f2246c57cc19d4c
magzhan <erbosynmagzan@gmail.com>
2026-08-27 18:05:12 +0500

    добавить хедер компонент

--- react-native/todo/src/app/index.tsx
+++ react-native/todo/src/app/index.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Header from '@/layout/header/Header';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle={'dark-content'} />
+      <Header />
       <Text>I finally made something to connect with ios</Text>
     </View>
   );
